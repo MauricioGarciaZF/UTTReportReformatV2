896ab0c0c4893ea09b95737b990bbdeedfe42ef5
MauricioGarciaZF <mauricio.garcia4@zf.com>
2025-04-01 16:18:06 -0600

    Reporte hecho

--- ut_report_reclassifier.py
+++ ut_report_reclassifier.py
@@ -3,6 +3,7 @@ data = pd.DataFrame
 from io import StringIO
 import csv
 import numpy as np
+import re
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
@@ -51,8 +52,11 @@ def read_project_directory (fileaddress,testnames) :
                 if str.upper(testnames[i])+".C" == str.upper(row[-1]): # Compare with the last value of the file address
                     writer.writerow(row) 
     column_names = [i for i in range(0, 11)]
+    
+    
     data = pd.DataFrame()
     data = pd.read_csv('writefile1.txt', sep=',', header=None,names=column_names)
+    data[10] = data[10].fillna(data[9])
     data["name"] = data[10].str.upper().str[:-2]
     data = data.rename(columns = {5:"Domain"})
     data = data.rename(columns = {6:"Subsystem"})
@@ -68,7 +72,7 @@ def clean_report_data (ut_report_df):
     """
     #test_types = ['TESTCASES','Statements','Branches','Pairs','Function Calls']
     clean_df = pd.DataFrame()
-    clean_df = ut_report_df
+    clean_df  = ut_report_df
     for i in test_types:
 
         clean_df[[i+"_NUMBER",i+"_PERCENTREPORT"]] = clean_df[i].str.split(" ",expand = True)
@@ -89,6 +93,36 @@ def clean_report_data (ut_report_df):
         #Remover columnas que no se van a utilizar, la origina y la de number (Agregar luego la de porcentajes)
     return clean_df
 
+def fix_time_format (merged_df):
+    
+    #hh_mm_ss_pattern = r"^\d{1,2}:\d{2}:\d{2}$"
+    mm_ss_pattern = r"^\d{1,2}:\d{2}$"
+    
+    buffer_df = pd.DataFrame()
+
+    buffer_df = merged_df
+    
+    
+    for i in merged_df.index:
+        
+        if re.match(mm_ss_pattern,buffer_df["Execution Time"].iloc[i]):
+            buffer_df["Execution Time"].iloc[i] = "00:"+buffer_df["Execution Time"].iloc[i].str()
+            
+        if re.match(mm_ss_pattern,buffer_df["Build Time"].iloc[i]):
+            buffer_df["Build Time"].iloc[i] = "00:"+buffer_df["Build Time"].iloc[i].str()
+
+    return buffer_df
+
+def check_time_format(value):
+    
+    hh_mm_ss_pattern = r"^\d{1,2}:\d{2}:\d{2}$"
+    mm_ss_pattern = r"^\d{1,2}:\d{2}$"
+
+    if re.match(hh_mm_ss_pattern, value):
+        return value
+    elif re.match(mm_ss_pattern, value):
+        return "00:"+value
+
 
 def reformat_time (merged_df):
     '''
@@ -102,13 +136,13 @@ def reformat_time (merged_df):
 
     buffer_df = merged_df
 
-    buffer_df["EXECUTE TIME"] =buffer_df["EXECUTE TIME"].replace('-',np.nan)
+    # buffer_df["Execution Time"] =buffer_df["Execution Time"].replace('-',np.nan)
 
-    buffer_df["EXECUTE TIME"] = pd.to_timedelta("00:"+buffer_df["EXECUTE TIME"])
+    buffer_df["Execution Time"] = pd.to_timedelta(buffer_df["Execution Time"]) 
 
-    buffer_df["BUILD TIME"] =buffer_df["BUILD TIME"].replace('-',np.nan)
+    # buffer_df["Build Time"] =buffer_df["Build Time"].replace('-',np.nan)
 
-    buffer_df["BUILD TIME"] = pd.to_timedelta("00:"+buffer_df["BUILD TIME"])
+    buffer_df["Build Time"] = pd.to_timedelta(buffer_df["Build Time"])
 
     return buffer_df
 
@@ -126,7 +160,7 @@ def summary_reclassified_df (merged_df):
     .set_index("name", append=True)
     )   
 
-    summary_subsistema[["name","BUILD"]] = np.nan
+    summary_subsistema["Build Status"] = np.nan
 
     summary_domain = ( #Hacer un data frame con sumas por subsistemas.
     merged_df.groupby(["Domain"])
@@ -137,13 +171,51 @@ def summary_reclassified_df (merged_df):
     .set_index("name", append=True)
     ) 
 
-    summary_domain[["name","BUILD"]] = np.nan
+    summary_domain["Build Status"] = np.nan
 
     merged_df = pd.concat([merged_df,summary_subsistema,summary_domain]).sort_index(level=["Domain", "Subsystem","name"],axis = 0) 
 
     return merged_df
 
-def make_reclassified_html(cleand_df):
+
+
+def make_clean_df (merged_df):
+    '''
+    Function: Join the three values of passed tests, total tests, and porcentage into a single string values and remove the days from the time format.
+
+
+    Parameters: 
+        merged_df: Dataframe obtained merging the source_files_df and ut_report_df
+
+    '''
+
+    clean_df = merged_df[base_columns_list]
+
+    for i in test_types: # WARNING fix
+        clean_df.loc[:,i] = merged_df.loc[:,i+"_PASSED"].astype(str)+"/"+merged_df.loc[:,i+"_TOTAL"].astype(str)+"("+merged_df.loc[:,i+'_PERCENT'].round(1).astype(str)+")"
+
+    clean_df['Build Time'] = clean_df['Build Time'].astype(str).str.slice(7) # Warning fix
+    clean_df['Execution Time'] = clean_df['Execution Time'].astype(str).str.slice(7)
+
+    return clean_df
+
+def make_style_df(merged_df):
+
+    '''
+    
+    '''
+
+    style_df = merged_df[base_columns_list]
+
+    for i in test_types:
+        style_df = pd.concat([style_df,merged_df[i+"_style"]],axis=1)
+
+    style_df.index=clean_df.index
+    style_df.columns=clean_df.columns
+
+    return style_df
+
+def make_reclassified_html(clean_df):
     '''
     Function: Join the styles of the report html with the reclasified dataframe html table, output an
 
@@ -167,42 +239,6 @@ def make_reclassified_html(cleand_df):
     with open('reclassified_ut_report.html','w', newline ='\n') as fout:
         fout.write(reclassified_html)
 
-def make_clean_df (merged_df):
-    '''
-    Function: Join the three values of passed tests, total tests, and porcentage into a single string values and remove the days from the time format.
-
-
-    Parameters: 
-        merged_df: Dataframe obtained merging the source_files_df and ut_report_df
-
-    '''
-
-    clean_df = merged_df[base_columns_list]
-
-    for i in test_types: # WARNING fix
-        clean_df.loc[:,i] = merged_df.loc[:,i+"_PASSED"].astype(str)+"/"+merged_df.loc[:,i+"_TOTAL"].astype(str)+"("+merged_df.loc[:,i+'_PERCENT'].round(1).astype(str)+")"
-
-    clean_df['BUILD TIME'] = clean_df['BUILD TIME'].astype(str).str.slice(7) # Warning fix
-    clean_df['EXECUTE TIME'] = clean_df['EXECUTE TIME'].astype(str).str.slice(7)
-
-    return clean_df
-
-def make_style_df(merged_df):
-
-    '''
-    
-    '''
-
-    style_df = merged_df[base_columns_list]
-
-    for i in test_types:
-        style_df = pd.concat([style_df,merged_df[i+"_style"]],axis=1)
-
-    style_df.index=clean_df.index
-    style_df.columns=clean_df.columns
-
-    return style_df
-
 ut_report_df = pd.DataFrame()
 
 '''
@@ -210,36 +246,60 @@ Variables with the addresses of the vectorcast html report and a .txt of directo
 '''
 
 report_html_file_add = 'SW_Unit_Test_full_status_report_X02_003_000.html'
+report_csv_file_add = 'test_results 1.csv'
 projectdirectorytxt = 'ALL_SOURCE_FILES.TXT'
 
-ut_report_df = read_ut_report_html(report_html_file_add) # Read the vectorcast html report to a dataframe
+# Leer el csv
+csv_df = pd.read_csv(report_csv_file_add) 
 
-test_index = ut_report_df[ut_report_df['name'].str.contains("UT_")].index # Select the index of only the lines the contain UT_ the files to be tested.
+csv_df = csv_df.drop(["Test Suite"],axis =1)# Quitar la primera columna
 
-ut_report_df = ut_report_df.iloc[test_index] # Select those lines to a new dataframe
 
-ut_report_df['name'] = ut_report_df['name'].str.slice(3) # Remove the ut from the name of the tests
+csv_df = csv_df.rename(columns ={"Environment (488)":"name"})
 
-ut_report_df = ut_report_df.reset_index() 
+csv_df['name'] = csv_df['name'].str.slice(3)
 
-testnames = ut_report_df[ut_report_df.columns[1]] # Extract the names of the tests 
+testnames = csv_df['name'] # Extract the names of the tests 
 
-source_files_df= read_project_directory(projectdirectorytxt,testnames) # Use the names of the tests to read the address of the source files contained in the report
 
-merged_df = pd.merge(source_files_df,ut_report_df,how="left", on = "name") # Join both dataframes, through the name of the file.
+source_files_df= read_project_directory(projectdirectorytxt,testnames) 
 
-merged_df.sort_values(by=['Domain','Subsystem']) # Organize data by Domain and subsystem
 
-test_types = ['TESTCASES','Statements','Branches','Pairs','Function Calls'] # List of the differents types of test
 
+
+# source_files_df["name"] = source_files_df["name"].fillna(source_files_df[9]) # Fill the names where the values are shifted 
+
+
+merged_df = pd.merge(source_files_df,csv_df,how="left", on = "name")
+
+
+
+merged_df.sort_values(by=['Domain','Subsystem'])
+
+
+
+test_types = ['Test Cases', 'Expected Values','Statement Coverage', 'Branch Coverage','Pair Coverage', 'Function Call Coverage']
 
 merged_df = clean_report_data(merged_df) # Separate string "1/1" into int data for later calculations
 
+
+merged_df["Execution Time"] = merged_df["Execution Time"].replace(np.nan,"00:00:00")
+merged_df["Build Time"] = merged_df["Build Time"].replace(np.nan,"00:00:00")
+
+merged_df["Execution Time"] = merged_df["Execution Time"].apply(check_time_format)
+
+merged_df["Build Time"] = merged_df["Build Time"].apply(check_time_format)
+
+
+
+
+
 merged_df = reformat_time(merged_df) # Reformat time string for later use in sums
 
+# 
 merged_df = summary_reclassified_df(merged_df)
 
-#Calculate porcentages
+
 for i in test_types:
     merged_df[i+'_PERCENT'] = (merged_df[i+"_PASSED"] / merged_df[i+"_TOTAL"]) * 100
 
@@ -249,7 +309,7 @@ for i in test_types:
     merged_df.loc[merged_df[i+"_PASSED"] < merged_df[i+"_TOTAL"], i+"_style"] = "danger"
 
 
-base_columns_list = ['BUILD', 'BUILD TIME','EXECUTE TIME'] 
+base_columns_list = ['Build Status', 'Build Time','Execution Time']
 
 
 clean_df = make_clean_df(merged_df) # Create dataframe with presntation for the final table
@@ -258,8 +318,11 @@ clean_df = make_clean_df(merged_df) # Create dataframe with presntation for the 
 # Make dataframe for the styles of the table
 style_df = make_style_df(merged_df)
 
+make_reclassified_html(clean_df)
+        
 
+# %%
 
-make_reclassified_html(clean_df) # Join both strings and output the reclassified html
+not_present = csv_df["name"][~csv_df["name"].isin(source_files_df["name"])]
 
-
+print(not_present)
